b6c756da9ec0f12aab5584d25c8a80c2aa626f8e
Clarissa Tatang <clarissat@canva.com>
2023-10-31 21:25:41 +1100

    add match/player logic to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Player } from './Player';
+import { PlayerName } from './matches/players';
+import { matchesS1 } from './matches/s1';
+import { Match } from './Match';
 
 function App() {
+  const players: Player[] = [];
+  const playerNames = Object.keys(PlayerName).filter((key) => isNaN(Number(key)));
+  playerNames.forEach((key, value) => {
+    players.push(new Player(value));
+  });
+
+  for (const matchDto of matchesS1) {
+    const match = new Match(matchDto);
+    match.updatePlayerData(players);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
