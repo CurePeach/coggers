a4f0a5db6f00bb2938a4757b489a6cfa9cdd29ed
Clarissa Tatang <clarissat@canva.com>
2023-10-31 20:30:57 +1100

    add convert function

--- src/matches/players.ts
+++ src/matches/players.ts
@@ -19,3 +19,46 @@ export enum PlayerName {
   Taikutsu,
   ThePretender,
 }
+
+export const convert = (key: PlayerName): string => {
+  switch (key) {
+    case PlayerName.Applesmustdie:
+      return 'Applesmustdie';
+    case PlayerName.Boundless:
+      return 'Boundless';
+    case PlayerName.CureLemonade:
+      return 'Cure Lemonade';
+    case PlayerName.Delucidationer:
+      return 'Delucidationer';
+    case PlayerName.Draghau:
+      return 'Draghau';
+    case PlayerName.edmododragon:
+      return 'edmododragon';
+    case PlayerName.ElVaux:
+      return 'ElVaux';
+    case PlayerName.JemLem:
+      return 'JemLem';
+    case PlayerName.NVRPY:
+      return 'NVRPY';
+    case PlayerName.NightRaven:
+      return 'NightRaven';
+    case PlayerName.RayOfGayshine:
+      return 'RayOfGayshine';
+    case PlayerName.Roland:
+      return 'Roland';
+    case PlayerName.SatanBlaze:
+      return 'Satan Blaze';
+    case PlayerName.Sewerslvt:
+      return 'Sewerslvt';
+    case PlayerName.Sharpaim:
+      return 'Sharpaim';
+    case PlayerName.summon3r000:
+      return 'summon3r000';
+    case PlayerName.summon3r003:
+      return 'summon3r003';
+    case PlayerName.Taikutsu:
+      return 'Taikutsu';
+    case PlayerName.ThePretender:
+      return 'The Pretender';
+  }
+};
