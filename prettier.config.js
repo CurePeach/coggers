5fd0e19025d0712ed6e2dc178735300625f99803
Clarissa Tatang <clarissat@canva.com>
2023-11-08 20:13:54 +1100

    add import order formatting

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,13 @@
 const config = {
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
   trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   printWidth: 100,
+  importOrder: ['^components/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
 };
 
 module.exports = config;
