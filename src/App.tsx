1c246fa22a3efec5f2f417aa18acb4a3d00ae546
Clarissa Tatang <clarissat@canva.com>
2023-11-11 20:28:00 +1100

    Preparing for Frontend (#10)
    * move updateXData to utils/match_helpers
    
    * delete useless react components that do nothing
    
    * App is now a const () => {}
    
    * move players/champions/matches logic into index.tsx
    
    * put instantiated matches into a list

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,9 @@
 import React from 'react';
 
-import { championIds } from 'data/champions';
-import { playerIds } from 'data/players';
-import { matchesS1 } from 'data/s1';
-
-import { ChampionStore } from 'champion/champion_store';
-import { MatchPresenter } from 'match/match_presenter';
-import { PlayerStore } from 'player/player_store';
-import { PlayerPairStore } from 'player_pair/player_pair_store';
-
 import './App.css';
 import logo from './logo.svg';
 
-function App() {
-  const players: PlayerStore[] = [];
-
-  const playerPairs: PlayerPairStore[] = [];
-  playerIds.forEach((id, index) => {
-    players.push(new PlayerStore(id));
-
-    const otherIds = playerIds.slice(index + 1);
-    for (const id2 of otherIds) {
-      playerPairs.push(new PlayerPairStore(id, id2));
-    }
-  });
-
-  const champions: ChampionStore[] = [];
-  championIds.forEach((id) => {
-    champions.push(new ChampionStore(id));
-  });
-
-  const matchPresenter = new MatchPresenter();
-  for (const matchDto of matchesS1) {
-    const match = matchPresenter.createMatchStore(matchDto);
-    matchPresenter.updatePlayerData(match, players);
-    matchPresenter.updatePlayerPairData(match, playerPairs);
-    matchPresenter.updateChampionData(match, champions);
-  }
-
-  for (const player of players) {
-    if (player.numGames > 0) {
-      player.print();
-    }
-  }
-
-  for (const pair of playerPairs) {
-    if (pair.numGames > 0) {
-      pair.print();
-    }
-  }
-
-  for (const champ of champions) {
-    if (champ.numPicks > 0 || champ.numBans > 0) {
-      champ.print();
-    }
-  }
-
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -74,6 +22,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
--- src/player/player.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export type PlayerProps = {
-  name: string;
-};
-
-export const Player = ({ name }: PlayerProps) => {
-  return <div>{name}</div>;
-};
