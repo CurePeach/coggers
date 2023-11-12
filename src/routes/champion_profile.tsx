386aa6d15a41e95866f3e2c35b23fac2a8b3fa8e
Clarissa Tatang <clarissat@canva.com>
2023-11-12 23:27:30 +1100

    import type

--- src/routes/champion_profile.tsx
+++ src/routes/champion_profile.tsx
@@ -2,17 +2,17 @@ import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import { LoaderData, useLoaderData } from 'react-router-typesafe';
 
-import { championProfileLoader } from 'utils/route_loaders';
+import type { championProfileLoader } from 'utils/route_loaders';
 
 export const ChampionProfile = () => {
   const data = useLoaderData() as LoaderData<typeof championProfileLoader>;
   const params = useParams();
-  
+
   const id = Number(params.championId);
   if (isNaN(id)) {
     return <div>Please provide a valid champion ID.</div>;
   }
-  
+
   const champ = data.champions[id];
 
   return (
