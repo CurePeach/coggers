3954f0e7bbebd838c79eb394b34e5d9ddebadd3b
Clarissa Tatang <clarissat@canva.com>
2023-11-16 00:14:52 +1100

    order champions in champ list based on how frequently they've been played

--- src/routes/players_list/ui/player_row.tsx
+++ src/routes/players_list/ui/player_row.tsx
@@ -9,13 +9,25 @@ import type { PlayerStore } from 'player/player_store';
 import styles from './player_row.module.css';
 
 export const PlayerRow = ({ player }: { player: PlayerStore }) => {
+  const champFrequencies: Record<string, number> = {};
+  player.scores.forEach((score) => {
+    const championName = convert(score.champion);
+    if (champFrequencies[championName]) {
+      champFrequencies[championName] += 1;
+    } else {
+      champFrequencies[championName] = 1;
+    }
+  });
+
   const champList: string[] = player.scores
     .map((score) => {
       return convert(score.champion);
     })
     .filter((value, index, self) => {
       return index === self.indexOf(value);
-    });
+    })
+    .sort()
+    .sort((a, b) => champFrequencies[b] - champFrequencies[a]);
 
   return (
     <div key={player.key} className={styles.playerRow}>
@@ -27,14 +39,8 @@ export const PlayerRow = ({ player }: { player: PlayerStore }) => {
           attribute="Win rate"
           value={`${(player.winRate * 100).toFixed(2)}% (${player.numWins} / ${player.numGames})`}
         />
-        <AttributeValuePair 
-          attribute="Number of MVPs"
-          value={player.numMvps.toString()}
-        />
-        <AttributeValuePair 
-          attribute="Number of Aces"
-          value={player.numAces.toString()}
-        />
+        <AttributeValuePair attribute="Number of MVPs" value={player.numMvps.toString()} />
+        <AttributeValuePair attribute="Number of Aces" value={player.numAces.toString()} />
         <AttributeValuePair
           attribute="Champions played"
           value={`${champList.join(', ')} (${champList.length})`}
