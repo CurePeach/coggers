be33c1e93f4a16b494645bb74e73de526537d417
Clarissa Tatang <clarissat@canva.com>
2023-11-15 23:29:00 +1100

    add css to put players into rows

--- src/routes/players_list/ui/player_row.tsx
+++ src/routes/players_list/ui/player_row.tsx
@@ -6,6 +6,8 @@ import { Subheading } from 'ui/typography';
 
 import type { PlayerStore } from 'player/player_store';
 
+import styles from './player_row.module.css';
+
 export const PlayerRow = ({ player }: { player: PlayerStore }) => {
   const champList: string[] = player.scores
     .map((score) => {
@@ -16,12 +18,16 @@ export const PlayerRow = ({ player }: { player: PlayerStore }) => {
     });
 
   return (
-    <div key={player.key}>
-      <Subheading>{player.playerName}</Subheading>
-      <div>
-        Winrate: {(player.winRate * 100).toFixed(2)} ({player.numWins} / {player.numGames})
+    <div key={player.key} className={styles.playerRow}>
+      <div className={styles.playerName}>
+        <Subheading>{player.playerName}</Subheading>
       </div>
-      <div>Champions played: {champList.join(', ')}</div>
+      <div className={styles.details}>
+        <div>
+          Winrate: {(player.winRate * 100).toFixed(2)} ({player.numWins} / {player.numGames})
+        </div>
+        <div>Champions played: {champList.join(', ')}</div>
+      </div>
     </div>
   );
 };
