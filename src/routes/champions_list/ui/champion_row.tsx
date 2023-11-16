21f708dd34d229dbabcf9a674eb5e26ca5f07743
Clarissa Tatang <clarissat@canva.com>
2023-11-16 20:35:48 +1100

    copy over player row css

--- src/routes/champions_list/ui/champion_row.tsx
+++ src/routes/champions_list/ui/champion_row.tsx
@@ -2,13 +2,15 @@ import { AttributeValuePair, Subheading } from 'ui/typography';
 
 import { ChampionStore } from 'champion/champion_store';
 
+import styles from './champion_row.module.css';
+
 export const ChampionRow = ({ champion }: { champion: ChampionStore }) => {
   return (
-    <div key={champion.key}>
-      <div>
+    <div key={champion.key} className={styles.championRow}>
+      <div className={styles.championName}>
         <Subheading>{champion.championName}</Subheading>
       </div>
-      <div>
+      <div className={styles.details}>
         <AttributeValuePair
           attribute="Win rate"
           value={`${(champion.winRate * 100).toFixed(2)} (${champion.numWins} / ${
