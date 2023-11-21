37725de90ca487f588818c8cfc99c9c0baaeb772
Clarissa Tatang <clarissat@canva.com>
2023-11-21 14:25:29 +1100

    add m1

---
+++ src/data/s3.ts
@@ -0,0 +1,156 @@
+import { ChampionName } from './champions';
+import { PlayerName } from './players';
+import { ExtraMatchProperties, MatchDto } from './types';
+
+export const matchesS3: (MatchDto & ExtraMatchProperties)[] = [
+  {
+    id: 301,
+    date: new Date('2023-07-04'),
+    effect: 'Unique Summoners',
+    win: 'red',
+    mvp: PlayerName.Draghau,
+    ace: PlayerName.summon3r000,
+    draft: {
+      bans: {
+        blue: [
+          [ChampionName.Aatrox],
+          [ChampionName.Warwick],
+          [ChampionName.RekSai],
+          [ChampionName.Twitch],
+          [ChampionName.Nilah],
+        ],
+        red: [
+          [ChampionName.JarvanIV],
+          [ChampionName.Shaco],
+          [ChampionName.Malphite],
+          [ChampionName.Quinn],
+          [ChampionName.Poppy],
+        ],
+      },
+      picks: {
+        blue: [
+          ChampionName.Xayah,
+          ChampionName.Mordekaiser,
+          ChampionName.Thresh,
+          ChampionName.Veigar,
+          ChampionName.Viego,
+        ],
+        red: [
+          ChampionName.Riven,
+          ChampionName.Rakan,
+          ChampionName.KhaZix,
+          ChampionName.Orianna,
+          ChampionName.Varus,
+        ],
+      },
+    },
+    teams: {
+      blue: {
+        captain: PlayerName.ThePretender,
+        players: [
+          {
+            name: PlayerName.JemLem,
+            champion: ChampionName.Mordekaiser,
+            role: 'top',
+            kills: 3,
+            deaths: 7,
+            assists: 9,
+            cs: 153,
+          },
+          {
+            name: PlayerName.ElVaux,
+            champion: ChampionName.Viego,
+            role: 'jungle',
+            kills: 3,
+            deaths: 11,
+            assists: 7,
+            cs: 138,
+          },
+          {
+            name: PlayerName.summon3r000,
+            champion: ChampionName.Veigar,
+            role: 'mid',
+            kills: 8,
+            deaths: 2,
+            assists: 7,
+            cs: 193,
+          },
+          {
+            name: PlayerName.CureLemonade,
+            champion: ChampionName.Xayah,
+            role: 'bot',
+            kills: 7,
+            deaths: 6,
+            assists: 11,
+            cs: 218,
+          },
+          {
+            name: PlayerName.ThePretender,
+            champion: ChampionName.Thresh,
+            role: 'support',
+            kills: 1,
+            deaths: 11,
+            assists: 18,
+            cs: 34,
+          },
+        ],
+      },
+      red: {
+        captain: PlayerName.NightRaven,
+        players: [
+          {
+            name: PlayerName.Roland,
+            champion: ChampionName.Riven,
+            role: 'top',
+            kills: 9,
+            deaths: 4,
+            assists: 7,
+            cs: 280,
+          },
+          {
+            name: PlayerName.Sharpaim,
+            champion: ChampionName.KhaZix,
+            role: 'jungle',
+            kills: 3,
+            deaths: 7,
+            assists: 13,
+            cs: 155,
+          },
+          {
+            name: PlayerName.Draghau,
+            champion: ChampionName.Orianna,
+            role: 'mid',
+            kills: 8,
+            deaths: 0,
+            assists: 12,
+            cs: 236,
+          },
+          {
+            name: PlayerName.NightRaven,
+            champion: ChampionName.Varus,
+            role: 'bot',
+            kills: 16,
+            deaths: 7,
+            assists: 12,
+            cs: 213,
+          },
+          {
+            name: PlayerName.RayOfGayshine,
+            champion: ChampionName.Rakan,
+            role: 'support',
+            kills: 1,
+            deaths: 5,
+            assists: 28,
+            cs: 30,
+          },
+        ],
+      },
+    },
+    duration: 29 * 60 + 53,
+    dragonSoul: 'mountain',
+    events: [
+      "Gordon didn't get his penta",
+      'David and Gordon used Barrier while trying to Teleport',
+    ],
+  },
+];
