a6c0108f02fa923389305c98814a6796d6a196ec
Clarissa Tatang <clarissat@canva.com>
2023-12-03 22:11:55 +1100

    get PlayerStore from params playerId

--- src/routes/player_profile/player_profile.tsx
+++ src/routes/player_profile/player_profile.tsx
@@ -1,5 +1,32 @@
 import * as React from 'react';
+import { useParams } from 'react-router-dom';
+import { LoaderData, useLoaderData } from 'react-router-typesafe';
+
+import { PlayerStore } from 'player/player_store';
+import { ErrorPage } from 'routes/error_page';
+import { Title } from 'ui/base/typography';
+import type { playerProfileLoader } from 'utils/route_loaders';
 
 export const PlayerProfile = () => {
-  return <div>Player Profile</div>;
+  const data = useLoaderData() as LoaderData<typeof playerProfileLoader>;
+  const params = useParams();
+
+  const rawPlayerId = params.playerId;
+  let player: PlayerStore;
+  try {
+    const playerId = Number(rawPlayerId);
+    player = data.players[playerId];
+    if (player === undefined) {
+      throw Error;
+    }
+  } catch (error) {
+    console.error(`Cannot find player with ID: ${rawPlayerId}`);
+    return <ErrorPage />;
+  }
+
+  return (
+    <div>
+      <Title>{player.playerName}</Title>
+    </div>
+  );
 };
