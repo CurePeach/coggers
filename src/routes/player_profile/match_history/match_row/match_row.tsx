e1f56b104338d9a54f82bf060898300ed1d4f4dd
Clarissa Tatang <clarissat@canva.com>
2023-12-03 22:32:43 +1100

    add css

--- src/routes/player_profile/match_history/match_row/match_row.tsx
+++ src/routes/player_profile/match_history/match_row/match_row.tsx
@@ -1,13 +1,15 @@
 import { convert } from 'data/champions';
 import type { ScoreStore } from 'score/score_store';
 
+import styles from './match_row.module.css';
+
 export type MatchRowProps = {
   score: ScoreStore;
 };
 
 export const MatchRow = ({ score }: MatchRowProps) => {
   return (
-    <div style={{ padding: '10px' }}>
+    <div className={styles.matchRow}>
       {convert(score.champion)} {score.kills}/{score.deaths}/{score.assists} {score.cs} CS
       <br />
       Role: {score.role}
