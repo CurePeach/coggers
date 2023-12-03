5ab095f697af48e487b706307f5ae86e9eea67fe
Clarissa Tatang <clarissat@canva.com>
2023-12-03 22:40:52 +1100

    clean up match row

--- src/routes/player_profile/match_history/match_row/match_row.tsx
+++ src/routes/player_profile/match_history/match_row/match_row.tsx
@@ -1,5 +1,5 @@
-import { convert } from 'data/champions';
 import type { ScoreStore } from 'score/score_store';
+import { ChampionIcon } from 'ui/base/champion_icon/champion_icon';
 
 import styles from './match_row.module.css';
 
@@ -10,11 +10,16 @@ export type MatchRowProps = {
 export const MatchRow = ({ score }: MatchRowProps) => {
   return (
     <div className={styles.matchRow}>
-      {convert(score.champion)} {score.kills}/{score.deaths}/{score.assists} {score.cs} CS
-      <br />
-      Role: {score.role}
-      <br />
-      Win: {score.win ? 'true' : 'false'}
+      <div>
+        <ChampionIcon championId={score.champion} size="small" />
+      </div>
+      <div>
+        {score.kills}/{score.deaths}/{score.assists}
+        <br />
+        {score.kda.toFixed(2)} KDA
+        <br />
+        {score.cs} CS
+      </div>
     </div>
   );
 };
